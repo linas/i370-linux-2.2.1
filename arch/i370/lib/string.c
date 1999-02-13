306c850da9cad6ea9b1489eff28e98b9d80c811c
Linas Vepstas <linas@linas.org>
1999-02-13 01:27:26 -0600

    oops

--- arch/i370/lib/string.c
+++ arch/i370/lib/string.c
@@ -12,7 +12,6 @@ int strlen (const char *head)
 
 int strcmp(const char *s1, const char *s2)
 {
-   int rc;
    int i=0;
    while (s1[i] && s2[i] && (s1[i] == s2[i])) i++;
 
@@ -22,10 +21,10 @@ int strcmp(const char *s1, const char *s2)
    return 0;
 }
 
-void *memset(void *s, int c, size_t n)
+void *memset(void *s, int c, int n)
 {
-   char *ss = (char *)s
-   size_t i=0;
+   char *ss = (char *)s;
+   int i=0;
    for (i=0; i<n; i++) {
       ss[i] = (char) c;
    }

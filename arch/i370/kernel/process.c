2814c09c998721319359d87c8214c397eb1b6db9
Linas Vepstas <linas@linas.org>
1999-02-14 00:33:07 -0600

    more hacking

--- arch/i370/kernel/process.c
+++ arch/i370/kernel/process.c
@@ -3,15 +3,46 @@
 #include <asm/pgtable.h>
 #include <asm/uaccess.h>
 
-// static struct vm_area_struct init_mmap = INIT_MMAP;
+static struct vm_area_struct init_mmap = INIT_MMAP;
 static struct fs_struct init_fs = INIT_FS;
 static struct file * init_fd_array[NR_OPEN] = { NULL, };
 static struct files_struct init_files = INIT_FILES;
 static struct signal_struct init_signals = INIT_SIGNALS;
 
-// struct mm_struct init_mm = INIT_MM;
+struct mm_struct init_mm = INIT_MM;
 union task_union init_task_union = { INIT_TASK };
 
 /* only used to get secondary processor up */
 // struct task_struct *current_set[NR_CPUS] = {&init_task, };
+
+
+void show_regs(struct pt_regs * regs)
+{
+	printk ("dohh show more dohh here ...\n");
+        printk("PSW-bits: %08lX PSW-ia: %08lX \n",
+               regs->psw_bits, regs->psw_ia);
+
+}
+
+void
+print_backtrace(unsigned long *sp)
+{
+//        int cnt = 0;
+//        unsigned long i;
+
+        printk("Call backtrace: ");
+#ifdef BOGUS_XXX
+        while (sp) {
+                if (__get_user( i, &sp[1] ))
+                        break;
+                if (cnt++ % 7 == 0)
+                        printk("\n");
+                printk("%08lX ", i);
+                if (cnt > 32) break;
+                if (__get_user(sp, (unsigned long **)sp))
+                        break;
+        }
+#endif
+        printk("\n");
+}
 
--- arch/i370/kernel/setup.c
+++ arch/i370/kernel/setup.c
@@ -9,14 +9,16 @@
 #include <asm/pgtable.h>
 #include <asm/processor.h>
 
+int _machine = 0;
+
 __initfunc(void setup_arch(char **cmdline_p,
 	unsigned long * memory_start_p, unsigned long * memory_end_p))
 {
 	extern int panic_timeout;
 	extern char _etext[], _edata[];
 	extern char *klimit;
-	extern unsigned long find_available_memory(void);
-	extern unsigned long *end_of_DRAM;
+//	extern unsigned long find_available_memory(void);
+// 	extern unsigned long *end_of_DRAM;
 
 	/* reboot on panic */	
 	panic_timeout = 180;
@@ -24,14 +26,14 @@ __initfunc(void setup_arch(char **cmdline_p,
 	init_task.mm->start_code = PAGE_OFFSET;
 	init_task.mm->end_code = (unsigned long) _etext;
 	init_task.mm->end_data = (unsigned long) _edata;
-	init_task.mm->brk = (unsigned long) klimit;	
+// 	init_task.mm->brk = (unsigned long) klimit;	
 
 	/* Save unparsed command line copy for /proc/cmdline */
 	// strcpy(saved_command_line, cmd_line);
 	// *cmdline_p = cmd_line;
 
-	*memory_start_p = find_available_memory();
-	*memory_end_p = (unsigned long) end_of_DRAM;
+// 	*memory_start_p = find_available_memory();
+//	*memory_end_p = (unsigned long) end_of_DRAM;
 
 	switch (_machine) {
 	default:

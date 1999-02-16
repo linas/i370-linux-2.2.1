a25f5896f40e77bc17c7f8aa045bde817c6fd3eb
Linas Vepstas <linas@linas.org>
1999-02-16 02:02:48 -0600

    misc hacks

---
+++ arch/i370/lib/misc.c
@@ -0,0 +1,17 @@
+
+/*
+ * Atomic add/sub/inc/dec operations
+ * Implement with test and set 
+ */
+
+void atomic_add(int c, int *v) {}
+void atomic_sub(int c, int *v) {}
+void atomic_inc(int *v) {}
+void atomic_dec(int *v) {}
+int atomic_dec_and_test(int *v) {}
+int atomic_inc_return(int *v) {}
+int atomic_dec_return(int *v) {}
+void atomic_clear_mask(atomic_t mask, atomic_t *addr) {}
+void atomic_set_mask(atomic_t mask, atomic_t *addr) {}
+
+

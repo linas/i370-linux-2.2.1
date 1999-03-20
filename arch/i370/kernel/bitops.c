528119da073b2af67de3bdf386c66d73f6476204
Linas Vepstas <linas@linas.org>
1999-03-20 02:39:25 -0600

    mplement atomic bitops

--- arch/i370/kernel/bitops.c
+++ arch/i370/kernel/bitops.c
@@ -1,31 +1,141 @@
 
+/*
+ * ESA/390 bitops
+ * Copied from the PowerPC bitops; these are atomic and SMP-safe.
+ * Copyright (c) 1999 Linas Vepstas
+ *
+ * XXX These should probably be moved to the header file and made inline.
+ * They are here right now for debugging & convenience purposes.
+ */
 #include <linux/kernel.h>
 #include <asm/bitops.h>
 
 void set_bit(int nr, volatile void *addr)
 {
+	volatile unsigned long oldval, newval;
+	unsigned long mask = 1 << (nr & 0x1f);
+	unsigned long *p = ((unsigned long *)addr) + (nr >> 5);
+
+	if ((unsigned long)addr & 3)
+		printk(KERN_ERR "set_bit(%x, %p)\n", nr, addr);        
+
+        __asm__ __volatile__("
+1:      L	%0,%2;
+	L	%1,%2;
+	OR	%1,%3;
+	CS	%0,%1,%2;
+        BNE	1b"
+        : "=r" (oldval), "=r" (newval), "=m" (*p)             
+        : "r" (mask)
+        : "memory");
 }
 
 void clear_bit(int nr, volatile void *addr)
 {
+	volatile unsigned long oldval, newval;
+	unsigned long mask = 1 << (nr & 0x1f);
+	unsigned long *p = ((unsigned long *)addr) + (nr >> 5);
+
+	if ((unsigned long)addr & 3)
+		printk(KERN_ERR "set_bit(%x, %p)\n", nr, addr);        
+
+	mask = ~mask;  /* complement */
+
+        __asm__ __volatile__("
+1:      L	%0,%2;
+	L	%1,%2;
+	NR	%1,%3;
+	CS	%0,%1,%2;
+        BNE	1b"
+        : "=r" (oldval), "=r" (newval), "=m" (*p)             
+        : "r" (mask)
+        : "memory");
 }
 
 void change_bit(int nr, volatile void *addr)
 {
+	volatile unsigned long oldval, newval;
+	unsigned long mask = 1 << (nr & 0x1f);
+	unsigned long *p = ((unsigned long *)addr) + (nr >> 5);
+
+	if ((unsigned long)addr & 3)
+		printk(KERN_ERR "set_bit(%x, %p)\n", nr, addr);        
+
+        __asm__ __volatile__("
+1:      L	%0,%2;
+	L	%1,%2;
+	XR	%1,%3;
+	CS	%0,%1,%2;
+        BNE	1b"
+        : "=r" (oldval), "=r" (newval), "=m" (*p)             
+        : "r" (mask)
+        : "memory");
 }
 
 int test_and_set_bit(int nr, volatile void *addr)
 {
-	return  0;
+	volatile unsigned long oldval, newval;
+	unsigned long mask = 1 << (nr & 0x1f);
+	unsigned long *p = ((unsigned long *)addr) + (nr >> 5);
+
+	if ((unsigned long)addr & 3)
+		printk(KERN_ERR "set_bit(%x, %p)\n", nr, addr);        
+
+        __asm__ __volatile__("
+1:      L	%0,%2;
+	L	%1,%2;
+	OR	%1,%3;
+	CS	%0,%1,%2;
+        BNE	1b"
+        : "=r" (oldval), "=r" (newval), "=m" (*p)             
+        : "r" (mask)
+        : "memory");
+
+	return  (oldval & mask);
 }
 
 int test_and_clear_bit(int nr, volatile void *addr)
 {
-	return 0;
+	volatile unsigned long oldval, newval;
+	unsigned long mask = 1 << (nr & 0x1f);
+	unsigned long maskcomp = ~mask;
+	unsigned long *p = ((unsigned long *)addr) + (nr >> 5);
+
+	if ((unsigned long)addr & 3)
+		printk(KERN_ERR "set_bit(%x, %p)\n", nr, addr);        
+
+        __asm__ __volatile__("
+1:      L	%0,%2;
+	L	%1,%2;
+	NR	%1,%3;
+	CS	%0,%1,%2;
+        BNE	1b"
+        : "=r" (oldval), "=r" (newval), "=m" (*p)             
+        : "r" (maskcomp)
+        : "memory");
+
+	return  (oldval & mask);
 }
 
 int test_and_change_bit(int nr, volatile void *addr)
 {
-	return 0;
+	volatile unsigned long oldval, newval;
+	unsigned long mask = 1 << (nr & 0x1f);
+	unsigned long *p = ((unsigned long *)addr) + (nr >> 5);
+
+	if ((unsigned long)addr & 3)
+		printk(KERN_ERR "set_bit(%x, %p)\n", nr, addr);        
+
+        __asm__ __volatile__("
+1:      L	%0,%2;
+	L	%1,%2;
+	XR	%1,%3;
+	CS	%0,%1,%2;
+        BNE	1b"
+        : "=r" (oldval), "=r" (newval), "=m" (*p)             
+        : "r" (mask)
+        : "memory");
+
+	return  (oldval & mask);
 }
 

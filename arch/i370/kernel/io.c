272e318587474acfc41f1862bdd92c396dfc8641
Linas Vepstas <linas@linas.org>
1999-03-24 21:03:27 -0600

    nitialize io

--- arch/i370/kernel/io.c
+++ arch/i370/kernel/io.c
@@ -11,7 +11,12 @@ unsigned int local_irq_count[NR_CPUS];
 
 __initfunc(void init_IRQ(void))
 {
-   printk ("init irq\n");
+	int i;
+	printk ("init irq\n");
+	for (i=0; i<NR_CPUS; i++) {
+		local_bh_count[i] = 0;
+		local_irq_count[i] = 0;
+	}
 }
 
 void
@@ -21,7 +26,7 @@ check_bugs(void)
 
 int get_irq_list(char *buf)
 {
-        int len = 0, j;
+        int len = 0;
         len += sprintf(buf+len, "No irqs XXX not implemented\n");
 	return len;
 }

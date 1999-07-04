c72c80c9d4cf05098789c7046a6d3d29e973e9a4
Linas Vepstas <linas@linas.org>
1999-07-04 00:53:23 -0500

    add misc copyin/copyout routines

--- arch/i370/kernel/mm.c
+++ arch/i370/kernel/mm.c
@@ -1,6 +1,6 @@
 
 /* 
- * XXX almost everythin in this file is wrong ...
+ * XXX some of what's in this file is very wrong ...
  */
 
 #include <linux/blk.h>
@@ -298,4 +298,128 @@ __initfunc(unsigned long paging_init(unsigned long start_mem,
 
 void set_context(int context) {}  
 
+/* ========================================================== */
+/* copy in/out routines of various sorts */
+
+int
+__copy_tofrom_user (void * to, const void * from, unsigned long len)
+{
+	/* XXX bogus  only works for KENREL_DS */
+	memcpy (to,from,len);
+	return 0;
+}
+int __strncpy_from_user(char *dst, const char *src, long count)
+{
+
+int     lclcount;
+
+	/* XXX bogus  only works for KENREL_DS */
+
+	lclcount = strlen(src);
+
+	strncpy (dst,src,count);
+
+	return lclcount;
+}
+
+ unsigned long __clear_user(void *addr, unsigned long size)
+{
+	/* set a block of bytes to zero */  
+	/* XXX bogus  only works for KENREL_DS */
+	memset (addr, 0, size);
+	return 0;
+}
+
+/*
+ * strlen_user(): Return the size of a string (including the ending 0)
+ *
+ * Return 0 for error
+ */
+
+long strlen_user(const char *str)
+{
+	/* XXX bogus  only works for KENREL_DS */
+	return strlen (str);
+}
+
+/* put_user_data() will only be called with len of 1,2 or 4 
+ * from uaccess.h */
+void 
+put_user_data(long data, void *addr, long len)
+{
+	pte_t *pte;
+	unsigned long va, ra, off;
+
+	/* find the page table entry for the addr */
+/* XXX note we need to figure out some way of walking the 
+ * the pte in some SMP-safe fashion, which find_pte isn't ...
+ */
+	va = (unsigned long) addr;
+	pte = find_pte (current->mm, va);
+	printk ("put_user_data: *(%x) = %x\n", pte, pte_val(*pte));
+
+/* XXX figure out how to map an umapped page */
+	if (pte_none(pte)) {
+		printk ("put_user_data: unmaped page \n");
+	   	while (1);
+	}
+
+	/* put together the real address */
+	off = va & ~PAGE_MASK;
+	ra = (pte_val(*pte) & PAGE_MASK) | off;
+
+	if (1 == len) {
+		char *ca = (char *) ra;
+		*ca = (char) (data & 0xff);
+	}
+
+	/* for length 2 or 4, data may be split over page boundry ... */
+	else
+	if (2 == len) {
+		if (0xfff > off) {
+			short *sa = (short *) ra;
+			*sa = (short) (data & 0xffff);
+		} else {
+			char *ca = (char *) ra;
+			*ca = (char) (data & 0xff);
+			put_user_data (data>>8, (void *) (va+1), 1);
+		}
+	}
+
+	else
+	if (4 == len) {
+		if (0xffd > off) {
+			long *la = (long *) ra;
+			*la = (long) data;
+		} else 
+		if (0xffd == off) {
+			short *sa = (short *) ra;
+			char *ca = (char *) (ra+2);
+			*sa = (short) (data & 0xffff);
+			*ca = (char) ((data>>16) & 0xff);
+			put_user_data (data>>24, (void *) (va+3), 1);
+		} else
+		if (0xffe == off) {
+			short *sa = (short *) ra;
+			*sa = (short) (data & 0xffff);
+			put_user_data (data>>16, (void *) (va+2), 2);
+		} else
+		{
+			char *ca = (char *) ra;
+			*ca = (char) (data & 0xff);
+			put_user_data (data>>8, (void *) (va+1), 3);
+		}
+	}
+
+	/* length 3 is always safe, since only called from above */
+	else
+	if (3 == len) {
+		short *sa = (short *) ra;
+		char *ca = (char *) (ra+2);
+		*sa = (short) (data & 0xffff);
+		*ca = (char) ((data>>16) & 0xff);
+	}
+}
+
+/* ============================ END OF FILE ============================== */
 

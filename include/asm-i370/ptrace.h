d3ac7983cda872d1f3d7f8d1d3cecaf9592166fc
Linas Vepstas <linas@linas.org>
1999-07-17 22:27:30 -0500

    added more regs to save

--- include/asm-i370/ptrace.h
+++ include/asm-i370/ptrace.h
@@ -153,6 +153,14 @@ struct _i370_irregs_s {
 	unsigned long	r0;
 	unsigned long	r1;
 	unsigned long	r2;
+	unsigned long	r3;
+	unsigned long	r4;
+	unsigned long	r5;
+	unsigned long	r6;
+	unsigned long	r7;
+	unsigned long	r8;
+	unsigned long	r9;
+	unsigned long	r10;
 };
 typedef struct _i370_irregs_s irregs_t;
 

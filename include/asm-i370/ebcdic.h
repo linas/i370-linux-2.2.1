44d860e233e4e31b1d3dd37402353d325fbd6de2
Linas Vepstas <linas@linas.org>
1999-11-07 23:08:17 -0600

    patches from Peter  <Peter.Schulte-Stracke@t-online.de> Date: Fri, 5 Nov 1999 22:50:47 +0100

--- include/asm-i370/ebcdic.h
+++ include/asm-i370/ebcdic.h
@@ -1,4 +1,4 @@
- /*       ebcdic .h -- Character set utilities 
+ /*       asm/ebcdic .h -- Character set utilities 
   
   This file is part of Linux/390.
 
@@ -8,29 +8,70 @@
   is permitted under the obligations of the GNU General Public Licence.
   See file COPYRIGHT for details. There is NO warranty.
 
- */ 
+  Usage: use i370_translate for generic operands, of fixed and
+  variable length, and with a table specified. For the default tables use 
+  i370_translate_to_ebcdic &c. NOTE that variable lengh operands with a 
+  length < 256 (! due to compiler/assembler bug for now) are much faster 
+  treated with __i370_translate[_to...], which modifies the translate 
+  instruction at run-time.
+
+  Date: $Id: ebcdic.h,v 1.4 1999/11/08 05:08:16 linas Exp $
+  Changes: Uses now test for constant lengh operand.
+
+  */ 
 
  
+
 #ifndef _I370_asm_EBCDIC_H
 #define _I370_asm_EBCDIC_H
 
-extern inline void
-__i370_translate (char * area, unsigned short size, const unsigned char table[256])   
+#define i370_translate(a, s, t) \
+if(__builtin_constant_p(s) && ((s) < 256))  { \
+ _constant_i370_translate((a), (s), (t)); } \
+ else {_i370_translate((a), (s), (t));}
+
+#define  _constant_i370_translate(area, size, table) \
+  __asm__ (" TR    %O0(%B2,%R0),%1;" \
+	   : "=m"(*area): "m"(*table), "I"(size))
+
+
+#define __i370_translate(area,size,table)\
+  __asm__ ("BCTR   %2,0;\n\t"\
+	   "EX %2,1f;\n\t"\
+	   "B 2f;\n1:\t"\
+	   "TR %O0(0,%R0),%1;\n2:\t;\n"\
+	  : "=m"(*area) : "m"(*table), "a"(size))
+
+
+static inline
+void  _i370_translate (char * area, size_t size, const char * table)
 {
-	/* XXX should check that size is 4095 or smaller */
-	__asm__ (" TR    %O0(%2,%R0),%1"
-		: "=m"(area)
-		: "m"(table), "I"(size));
+       register int rest, amount;
+       for (rest = size; rest > 0; rest -= 256)
+           { 
+               amount = (rest>256) ? 256 : rest;
+               __i370_translate(area, amount, table);
+               area += 256;
+           };
 }
 
+extern unsigned char tables_ebcdic_to_ascii [256];
+extern unsigned char tables_ascii_to_ebcdic [256];
+
 #define __i370_translate_to_ascii(area,size)\
-        __i370_translate(area,size,ebcdic_to_ascii)
+        __i370_translate(area,size,tables_ebcdic_to_ascii)
 #define __i370_translate_to_ebcdic(area,size)\
-        __i370_translate(area,size,ascii_to_ebcdic)
+        __i370_translate(area,size,tables_ascii_to_ebcdic)
 
-extern unsigned char ebcdic_to_ascii [256];
-extern unsigned char ascii_to_ebcdic [256];
+#define _i370_translate_to_ascii(area,size)\
+        _i370_translate(area,size,tables_ebcdic_to_ascii)
+#define _i370_translate_to_ebcdic(area,size)\
+        _i370_translate(area,size,tables_ascii_to_ebcdic)
 
+#define i370_translate_to_ascii(area,size)\
+        i370_translate(area,size,tables_ebcdic_to_ascii)
+#define i370_translate_to_ebcdic(area,size)\
+        i370_translate(area,size,tables_ascii_to_ebcdic)
 
 #endif  /* _I370_asm_EBCDIC_H */
 
--- include/asm-i370/string.h
+++ include/asm-i370/string.h
@@ -1,5 +1,5 @@
  /*
-        string.h -- String inlines 
+        asm/string.h -- String inlines 
 
   This file is part of Linux/390.
 
@@ -9,10 +9,14 @@
   is permitted under the obligations of the GNU General Public Licence.
   See file COPYRIGHT for details. There is NO warranty.
 
-  Date: $Id: string.h,v 1.4 1999/10/06 03:48:18 linas Exp $
+  Date: $Id: string.h,v 1.5 1999/11/08 05:08:17 linas Exp $
   Known Bugs: Generally, it is not checked for architecturally given
         length restrictions. 
-  Changes:
+  Changes: corrected #defines for inlines not provided; corrected
+        register usage in memcmp (ptr 1999-09-27)
+	Corrected operand template in i370_constant_memcpy, made constant 
+	expressions out of constant arguments in bzero, bcopy, memset, 
+	changed code for strcmp
 
  */ 
 
@@ -24,28 +28,23 @@
   .     a sufficient time of debugging and to add more generally useable
   .     versions a bit later. After all, the arch/i370/lib/string.c versions
   .     should be no longer useful; we could try to make these here `static'.
-  .     The __asm__ interface is not fully satisfactoring, if improvement
-  .     should be possible, 
+  .     Should rewrite this for extended versions of Move/Compare.
  */
  
 #ifndef _I370_STRING_H_
 #define _I370_STRING_H_
-#define __HAVE_ARCH_STRCPY
-#define __HAVE_ARCH_STRNCPY
-#define __HAVE_ARCH_STRLEN
-#define __HAVE_ARCH_STRCMP
-#define __HAVE_ARCH_STRCAT
-#define __HAVE_ARCH_MEMSET
-#define __HAVE_ARCH_BCOPY
-#define __HAVE_ARCH_MEMCPY
-#define __HAVE_ARCH_MEMMOVE
-#define __HAVE_ARCH_MEMCMP
-#define __HAVE_ARCH_MEMCHR
+
+#define __HAVE_ARCH_STRNCPY	/* in arch/i370/lib/string.c */
+//#define __HAVE_ARCH_STRCAT
+//#define __HAVE_ARCH_BCOPY
+//#define __HAVE_ARCH_MEMMOVE
+//#define __HAVE_ARCH_MEMCHR
 
 #include <linux/autoconf.h>
-
+#include <linux/types.h>
 #ifdef CONFIG_STRING_INLINES
 
+#define __HAVE_ARCH_MEMCMP
  
 extern inline
 int memcmp(const void *vs1, const void *vs2, size_t n) 
@@ -53,64 +52,90 @@ int memcmp(const void *vs1, const void *vs2, size_t n)
   register long  length __asm__ ("r1")  =  (long) n;
   register long  source __asm__ ("r14") =  (long) vs2;
  
-  __asm__ (" .set _i370_implied_op1,%0;
-             .set _i370_implied_op3,%1;
-             .set _i370_implied_op2,%2;             
-             LR   r15,r1;
-             CLCL r0,r14;
-             BZ   3f;
-             BM   1f;
-             LA   r1,1(0,0);
-             B    3f;
-        1:   BCTR r1,0;
-        3: "  
-	   ::"r"(target), "r"(length), "r"(source)
-	   :"cc",  "r15" );
+  __asm__ __volatile__ (".set _i370_implied_op1,%0;\n\t
+             .set _i370_implied_op3,%1;\n\t
+             .set _i370_implied_op2,%2;\n\t             
+             LR   r15,r1;\n\t
+             CLCL r0,r14;\n\t
+             BZ   3f;\n\t
+             BM   1f;\n\t
+             LA   r1,1(0,0);\n\t
+             B    3f;\n
+1:\tBCTR r1,0;
+3:\t;"
+	   :"+r"(target), "+r"(length), "+r"(source)
+	   ::"cc",  "r15" );
   return length;		/* contains now the code */
 };
 
+#define __HAVE_ARCH_MEMCPY
+
+#define memcpy(t, s, n) \
+if(__builtin_constant_p(n) && ((n) < 256)) { \
+       i370_bcopy((t), (s), (n)) ;} \
+else { i370_memcpy((t), (s), (n)); }
+
 extern inline
-void* memcpy (void *dest, const void *src, size_t n)
+void* i370_memcpy (void *dest, const void *src, __kernel_size_t n)
 {
   register long target __asm__ ("r0")  =  (long) dest;
   register long length __asm__ ("r1")  =  (long) n;
   register long source __asm__ ("r14") =  (long) src;
-  __asm__ (" .set _i370_implied_op1,%0;
-             .set _i370_implied_op3,%1;
-             .set _i370_implied_op2,%2;             
-             LR   r15,%0;
+  __asm__ __volatile__(".set _i370_implied_op1,%0;\n\t
+             .set _i370_implied_op3,%1;\n\t
+             .set _i370_implied_op2,%2;\n\t             
+             LR   r15,%1;\n\t
              MVCL r0,r14;"
-	   :: "r"(target),  "r"(length), "r"(source)
-	   :"cc",  "r0", "r1", "r15" );
+	   : "+r"(target),  "+r"(length), "+r"(source)
+	   ::"cc",  "r0", "r1", "r14", "r15" );
   
    return dest;
 };
 
-							   
+#define i370_bcopy(t, s, n) \
+  __asm__ __volatile__("MVC\t%O0(%B1,%R0),%2" \
+                     : "=m"(*(char*)t) : "I"(n+0), "m"(*(char*)s))
+
+
+#define __HAVE_ARCH_MEMSET
+#define memset(t, c, n) \
+if(__builtin_constant_p(n) && ((n) < 256)) { \
+      if (__builtin_constant_p(c) && ((c) == 0)) { \
+	     i370_bzero ((t), (n));} \
+      else { i370_constant_memset((t) , (c), (n));}} \
+ else { i370_memset((t), (c), (n));}						      
+
 extern inline 
-void *memset(void *s, int c, size_t n)
+void *i370_memset(void *s, int c, size_t n)
 {
   register long target  __asm__ ("0")  =  (long) s;
   register long length  __asm__ ("1")  =  (long) n;
   register long padding __asm__ ("15") =  (long) c << 24;
-  	/* not used :  r14 */
+  	/* r14  ignored */
 
-  __asm__ (" .set _i370_implied_op1,%0;
-             .set _i370_implied_op3,%1;
-             .set _i370_implied_op2,%2;             
+  __asm__ __volatile__(".set _i370_implied_op1,%0;\n\t
+             .set _i370_implied_op3,%1;\n\t
+             .set _i370_implied_op2,%2; \n\t            
              MVCL r0,r14;"
-	   :: "r"(target), "r"(length), "r"(padding)
+	   : "+r"(target), "+r"(length): "r"(padding)
 	   :"cc" );
     return s;
 }
+#define i370_bzero(area, size)\
+   __asm__ __volatile__("XC   %O0(%B1,%R0),%0" : "=m"(*(char*)area) : "I"(size+0))
 
+#define i370_constant_memset(area, chr , size)\
+   __asm__ __volatile__ ("MVI %0,%B1;\n\tMVC %O0+1(%B2,%R0),%0"\
+                        : "=m"(*(char*)area) : "I"(chr+0), "I"(size-1))
 #ifdef CONFIG_STRING_FEATURE
+
+#define __HAVE_ARCH_STRCMP
 
 extern inline 
 int strcmp(const char *s1, const char *s2)
 {
-   register int result __asm__("r0");
-  __asm__(".set  _i379_implied_op1,%0;
+ /*   register int result __asm__("r0");
+   __asm__ __volatile__(".set  _i379_implied_op1,%0;
            SLR   r0,r0;
        1:  CLST  %1,%2; 
            BZ    3f; 
@@ -119,25 +144,38 @@ int strcmp(const char *s1, const char *s2)
            BCTR  r0,r0;
            B     3f;
        2:  LA    r0,1(0,0);
-       3:    " : "=r"(result): "r"(s1), "r"(s2));
+       3:    " : "=r"(result): "r"(s1), "r"(s2));*/
+  int result;
+  __asm__ __volatile__ (
+	 "LA      %0,1(0,0);\n1:\t"
+	 "CLST    %1,%2;\n\t"
+	 "BP      3f;\n\t"
+	 "BM      2f;\n\t"
+	 "BO      1b;\n\t"
+	 "SLR     %0,%0;\n2:\t"
+	 "LCR     %0,%0;\n3:\n"
+	 : "=r"(result): "r"(s1), "r"(s2)); 
   return result;
 };
 
+#define __HAVE_ARCH_STRCPY
 
 extern inline 
 char* strcpy (char *dest, const char *src)
 {
-  __asm__("1: SLR r0,r0; MVST %0, %1; BO 1b; "
+  __asm__ __volatile__("1: SLR r0,r0; MVST %0, %1; BO 1b; "
 	  :: "r"(dest), "r"(src):
 	  "r0" , "cc");
   return dest;
 };
 
+#define __HAVE_ARCH_STRLEN
+
 extern inline
 size_t strlen (const char *head) 
 {
   register int length __asm__("r0");
-  __asm__(".set  _i370_implied_op1,%0;
+  __asm__ __volatile__(".set  _i370_implied_op1,%0;
            SLR    r0,r0;
       1:   SRST   r0,%1;
            BO     1b;
@@ -150,6 +188,8 @@ size_t strlen (const char *head)
 #endif /* STRING_FEATURE */
 
 extern int strcasecmp(const char *, const char *);
+extern void *memchr (const void *,int, size_t);
+
 
 #endif /* STRING_H */
 

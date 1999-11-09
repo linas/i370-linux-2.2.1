43bd01c1492069be3e1a7498e79e0837a014e1fc
Linas Vepstas <linas@linas.org>
1999-11-09 00:46:32 -0600

    remove a bunch of stuff, given given peter's hacking

--- arch/i370/lib/string.c
+++ arch/i370/lib/string.c
@@ -6,51 +6,9 @@
  * kernel.
  */
 
-typedef int size_t;
+typedef int __kernel_size_t;
 
-int memcmp(const void *vs1, const void *vs2, size_t n) 
-{
-   int i=0;
-   char * s1 = (char *) vs1;
-   char * s2 = (char *) vs2;
-
-   while ((s1[i] == s2[i]) && (i<n)) i++;
-
-   if (i == n) return 0;
-   if (s1[i] != s2[i]) {
-       return (s1[i] < s2[i]) ? 1 : -1 ;
-   } 
-   return 0;
-}
-
-
-void* memcpy (void *dest, const void *src, size_t n)
-{
-   int i=0;
-   char *d, *s;
-   d = (char *)dest;
-   s = (char *) src;
-   // rewrite to inline MVCL
-   for (i=0; i<n; i++) {
-      d[i] = s[i];
-   }
-   return dest;
-}
-
-void* memmove (void *dest, const void *src, size_t n)
-{
-   int i=0;
-   char *d, *s;
-   d = (char *)dest;
-   s = (char *) src;
-   // rewrite to inline MVCL
-   for (i=0; i<n; i++) {
-      d[i] = s[i];
-   }
-   return dest;
-}
-
-void *memset(void *s, int c, size_t n)
+void *memset(void *s, int c, __kernel_size_t n)
 {
    char *ss = (char *)s;
    int i=0;
@@ -60,7 +18,7 @@ void *memset(void *s, int c, size_t n)
    return s;
 }
 
-void *memchr(const void *s, int c, size_t n)
+void *memchr(const void *s, int c, __kernel_size_t n)
 {
    const char *ss = (const char *)s;
    int i=0;
@@ -70,35 +28,7 @@ void *memchr(const void *s, int c, size_t n)
    return 0x0;
 }
 
-int strcmp(const char *s1, const char *s2)
-{
-   int i=0;
-   while (s1[i] && s2[i] && (s1[i] == s2[i])) i++;
-
-   if (s1[i] != s2[i]) {
-       return (s1[i] < s2[i]) ? 1 : -1 ;
-   } 
-   return 0;
-}
-
-char* strcpy (char *dest, const char *src)
-{
-   int i=-1;
-   do {
-      i++;
-      dest[i] = src[i];
-   } while (src[i]);
-   return dest;
-}
-
-int strlen (const char *head) 
-{
-   char * tail = (char *) head;
-   while (*tail) tail ++;
-   return (tail - head);
-}
-
-char* strncpy (char *dest, const char *src, size_t n)
+char* strncpy (char *dest, const char *src, __kernel_size_t n)
 {
    int i=-1;
    n --;

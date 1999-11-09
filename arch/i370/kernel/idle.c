0c6b38843e7f8d359a5fcb1265486cd9ae7da83e
Linas Vepstas <linas@linas.org>
1999-11-09 00:45:08 -0600

    add a note

--- arch/i370/kernel/idle.c
+++ arch/i370/kernel/idle.c
@@ -57,7 +57,8 @@ int idled(void *unused)
 #endif
 		schedule();
 
-		/* if we came back here, then load an enabled wait */
+		/* If we came back here, then yeild the cpu with an enabled wait */
+		/* This seems appropriate both for VM and LPAR boots */
 		i370_enabled_wait();
 
 	}

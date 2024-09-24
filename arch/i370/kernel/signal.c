0bb1fac6f519ef7ebff6e8e1e07063d8894c54e4
Linas Vepštas <linasvepstas@gmail.com>
2024-09-24 17:17:04 -0500

    Prevent an inf loop

--- arch/i370/kernel/signal.c
+++ arch/i370/kernel/signal.c
@@ -2,9 +2,11 @@
 /* XXX signal.c not implemented. Take a look at ppc/signal.h for ideas. */
 
 #include <linux/kernel.h>
+#include <asm-i370/asm.h>
 
 void
 i370_do_signal (void)
 {
 	printk ("called do_signal XXX not implemented \n");
+	i370_halt();
 }

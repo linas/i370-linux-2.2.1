9aa549ce4b5d7343c081fe0981a0a65db93c66d3
Linas Vepštas <linasvepstas@gmail.com>
2024-09-25 22:21:44 -0500

    Try a less confusing print order

--- fs/binfmt_elf.c
+++ fs/binfmt_elf.c
@@ -739,12 +739,12 @@ do_load_elf_binary(struct linux_binprm * bprm, struct pt_regs * regs)
 	padzero(elf_bss);
 
 #if 1
-	printk("(start_brk) %x\n" , current->mm->start_brk);
-	printk("(end_code) %x\n" , current->mm->end_code);
 	printk("(start_code) %x\n" , current->mm->start_code);
+	printk("(end_code) %x\n" , current->mm->end_code);
 	printk("(end_data) %x\n" , current->mm->end_data);
-	printk("(start_stack) %x\n" , current->mm->start_stack);
+	printk("(start_brk) %x\n" , current->mm->start_brk);
 	printk("(brk) %x\n" , current->mm->brk);
+	printk("(start_stack) %x\n" , current->mm->start_stack);
 #endif
 
 	if ( current->personality == PER_SVR4 )

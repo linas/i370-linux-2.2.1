e05fe9076410520e12681554a341c1196dfaf95d
Linas Vepštas <linasvepstas@gmail.com>
2024-09-30 15:56:55 -0500

    Whitespace and header file cleanup

--- arch/i370/kernel/idle.c
+++ arch/i370/kernel/idle.c
@@ -1,8 +1,7 @@
 /*
- *
  * Idle daemon for I370.  Idle daemon will handle any action
  * that needs to be taken when the system becomes idle.
-
+ *
  * copied from linux/arch/ppc/kernel/idle.c
  *
  * This program is free software; you can redistribute it and/or
@@ -13,24 +12,13 @@
 #include <linux/config.h>
 #include <linux/errno.h>
 #include <linux/kernel.h>
-#include <linux/malloc.h>
-#include <linux/mm.h>
-#include <linux/ptrace.h>
 #include <linux/smp.h>
 #include <linux/smp_lock.h>
 #include <linux/sched.h>
 #include <linux/stddef.h>
-#include <linux/unistd.h>
 
 #include <asm/asm.h>
-#include <asm/cache.h>
 #include <asm/current.h>
-#include <asm/io.h>
-#include <asm/mmu.h>
-#include <asm/pgtable.h>
-#include <asm/processor.h>
-#include <asm/system.h>
-#include <asm/uaccess.h>
 
 void zero_paged(void);
 unsigned long zero_paged_on = 0;
@@ -43,11 +31,11 @@ int idled(void *unused)
 	for (;;)
 	{
 		__sti();
-		
+
 		/* check_pgt_cache(); */
 
 /*
- * It might be interesting to copy the zero_paged code from 
+ * It might be interesting to copy the zero_paged code from
  * the powerpc directory to here ... sounds like a good idea ...
 		if ( !current->need_resched && zero_paged_on ) zero_paged();
 */
@@ -60,7 +48,6 @@ int idled(void *unused)
 		/* If we came back here, then yeild the cpu with an enabled wait */
 		/* This seems appropriate both for VM and LPAR boots */
 		i370_enabled_wait();
-
 	}
 	return 0;
 }
@@ -73,7 +60,7 @@ int idled(void *unused)
 int cpu_idle(void *unused)
 {
 	idled(unused);
-	return 0; 
+	return 0;
 }
 #endif /* __SMP__ */
 
@@ -88,4 +75,3 @@ asmlinkage int i370_sys_idle(void)
 	idled(NULL);
 	return 0; /* should never execute this but it makes gcc happy -- Cort */
 }
-

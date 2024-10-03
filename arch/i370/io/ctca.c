17be55a41b15fab8c4ac3d80d13a81c1025208a4
Linas Vepštas <linasvepstas@gmail.com>
2024-10-03 17:50:54 -0500

    More stubbing of ctca

--- arch/i370/io/ctca.c
+++ arch/i370/io/ctca.c
@@ -51,56 +51,12 @@
 /*							  */
 /************************************************************/
 
+#include <asm/delay.h>
+#include <asm/iorb.h>
+#include <asm/spinlock.h>
+#include <asm/unitblk.h>
+
 #include <linux/fs.h>
-
-/*================== End of Include Statements =============*/
-
-/************************************************************/
-/*							  */
-/*		TYPE DEFINITIONS			  */
-/*		----------------			  */
-/*							  */
-/************************************************************/
-
-
-
-/*================== End of Type Definitions ===============*/
-
-/************************************************************/
-/*							  */
-/*	       FUNCTION PROTOTYPES			*/
-/*	       -------------------			*/
-/*							  */
-/************************************************************/
-
-/*================== End of Prototypes =====================*/
-
-/************************************************************/
-/*							  */
-/*	     GLOBAL VARIABLE DECLARATIONS		 */
-/*	     ----------------------------		 */
-/*							  */
-/************************************************************/
-
-struct file_operations i370_fop_ctca =
-{
-   NULL,		 /* lseek - default */
-   NULL,		 /* read - general block-dev read */
-   NULL,		 /* write - general block-dev write */
-   NULL,		 /* readdir - bad */
-   NULL,		 /* poll */
-   NULL,		 /* ioctl */
-   NULL,		 /* mmap */
-   NULL,		 /* open */
-   NULL,		 /* flush */
-   NULL,		 /* release */
-   NULL,		 /* fsync */
-   NULL,		 /* fasync */
-   NULL,		 /* check_media_change */
-   NULL,		 /* revalidate */
-};
-
-/*============== End of Variable Declarations ==============*/
 
 /************************************************************/
 /*							  */
@@ -109,10 +65,35 @@ struct file_operations i370_fop_ctca =
 /*							  */
 /************************************************************/
 
-void
-i370_ctca_driver(void)
+/* FIXME. The IOCCW needs to stay unclobbered until the I/O operation
+ * has completed, i.e. until after we get the secondary status interrupt.
+ * Until then, we should be careful not to clobber this!
+ * The current design is to just spin.
+ */
+static int align_ccw[5];
+ccw_t *ioccw;
+
+int i370_ctca_open (struct inode *inode, struct file *filp)
 {
-	printk("Hello CTCA\n");
+   printk ("ctca_open of /dev/%s (c %d %d)\n",
+           filp->f_dentry->d_iname,
+           inode->i_rdev >> 8, inode->i_rdev & 0xff);
+
+	/* double-word align the ccw array */
+	ioccw = (ccw_t *) (((((unsigned long) &align_ccw[0]) + 7) >>3) << 3);
+	return 0;
+}
+
+ssize_t i370_ctca_write (struct file *filp, const char *str,
+                         size_t len, loff_t *ignore)
+{
+	return 0;
+}
+
+ssize_t i370_ctca_read (struct file *filp, char *str,
+                        size_t len, loff_t *ignore)
+{
+	return 0;
 }
 
 /*===================== End of Mainline ====================*/
@@ -132,5 +113,25 @@ void
 i370_ctca_flih(int irq, void *dev_id, struct pt_regs *regs)
 {
 }
+
+/*================== End of Handler ========================*/
+
+struct file_operations i370_fop_ctca =
+{
+   NULL,		 /* lseek - default */
+   i370_ctca_read,	 /* read - general block-dev read */
+   i370_ctca_write,	 /* write - general block-dev write */
+   NULL,		 /* readdir - bad */
+   NULL,		 /* poll */
+   NULL,		 /* ioctl */
+   NULL,		 /* mmap */
+   i370_ctca_open, /* open */
+   NULL,		 /* flush */
+   NULL,		 /* release */
+   NULL,		 /* fsync */
+   NULL,		 /* fasync */
+   NULL,		 /* check_media_change */
+   NULL,		 /* revalidate */
+};
 
 /*===================== End of Function ====================*/
